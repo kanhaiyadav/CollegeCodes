646efe1bc349d784a0e56c954590aa9e9517fc00
Kanhaiya Yadav <kanhaiya2004yadav@gmail.com>
2025-02-04 13:03:51 +0530

    day3 p1.cpp

---
+++ oops/day3/p1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+private:
+    int value;
+
+public:
+    Counter(int v = 0) : value(v) {}
+
+    void display() const
+    {
+        cout << "Value: " << value << endl;
+    }
+
+    Counter &operator++()
+    {
+        ++value;
+        return *this;
+    }
+
+    Counter operator++(int)
+    {
+        Counter temp = *this;
+        value++;
+        return temp;
+    }
+
+    Counter &operator--()
+    {
+        --value;
+        return *this;
+    }
+
+    Counter operator--(int)
+    {
+        Counter temp = *this;
+        value--;
+        return temp;
+    }
+};
+
+int main()
+{
+    int n;
+    cout << "Enter the initial value of the counter: ";
+    cin >> n;
+    Counter obj(n);
+
+    cout << "Initial ";
+    obj.display();
+
+    cout << "\nAfter prefix increment (++obj): \n";
+    (++obj).display();
+
+    cout << "After postfix increment (obj++):\ncurrent ";
+    (obj++).display();
+    cout << "new ";
+    obj.display();
+
+    cout << "After prefix decrement (--obj): \n";
+    (--obj).display();
+
+    cout << "After postfix decrement (obj--):\ncurrent ";
+    (obj--).display();
+    cout << "new ";
+    obj.display();
+
+    return 0;
+}
